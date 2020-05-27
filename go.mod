c3e8d2a89cc81043704a44bb52eea9dac2bdd59d
Aaron Crowder <aaron.crowder@gmail.com>
2020-05-26 21:48:53 -0600

    Adds logger and config

--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module cloud.bsdrocker.com/CrowderSoup/di-example
 
 go 1.14
 
-require go.uber.org/fx v1.12.0
+require (
+	github.com/fatih/camelcase v1.0.0 // indirect
+	github.com/fatih/structs v1.1.0 // indirect
+	github.com/koding/multiconfig v0.0.0-20171124222453-69c27309b2d7
+	go.uber.org/fx v1.12.0
+	go.uber.org/zap v1.15.0
+)
--- main.go
+++ main.go
@@ -1,15 +1,36 @@
 package main
 
 import (
-	"log"
 	"net/http"
+
+	"github.com/koding/multiconfig"
+	"go.uber.org/zap"
 )
 
+// Config our app config
+type Config struct {
+	Address string `default:":3000"`
+}
+
 func main() {
+	var config Config
+	m := multiconfig.New()
+	m.MustLoad(&config)
+
+	logger, _ := zap.NewProduction()
+	defer logger.Sync() // flushes buffer, if any
+	sugar := logger.Sugar()
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		sugar.Infow("recieved request",
+			"RequestURI", r.RequestURI,
+		)
 		w.WriteHeader(200)
 		w.Write([]byte("Hello World"))
 	})
 
-	log.Fatal(http.ListenAndServe(":3000", http.DefaultServeMux))
+	sugar.Infow("starting http server",
+		"Address", config.Address,
+	)
+	sugar.Fatal(http.ListenAndServe(config.Address, http.DefaultServeMux))
 }
